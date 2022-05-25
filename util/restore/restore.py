bcae4b629400f43b08b263250882f6fb044eb4f7
EC2 Default User <ec2-user@ip-172-31-91-253.ec2.internal>
2022-05-25 06:13:12 +0000

    Finished for the night, wish me luck

--- util/restore/restore.py
+++ util/restore/restore.py
@@ -15,10 +15,94 @@ sys.path.insert(1, os.path.realpath(os.path.pardir))
 import helpers
 
 # Get configuration
-from configparser import SafeConfigParser
-config = SafeConfigParser(os.environ)
-config.read('notify_config.ini')
+CONFIG_FILE = '/home/ec2-user/mpcs-cc/gas/util/archive/restore_config.ini'
+config = ConfigParser()
+config.read_file(open(CONFIG_FILE))
+glacier = boto3.client('glacier')
 
 # Add utility code here
+def main(): 
+    while True: 
+        print('... checking for users to thaw ...')
+        response = sqs.receive_message(
+            QueueUrl=config.get('aws', 'SQSRestoreQueueUrl'), 
+            MaxNumberOfMessages=1, 
+            WaitTimeSeconds=20)
+        try:  # Receive message
+            message = response['Messages'][0]
+            message_body = message['Body']
+            receipt_handle = message['ReceiptHandle']
+        except KeyError: # No messages in queue
+            continue
+        actual_message = ast.literal_eval(message_body)
+        
+        try: 
+            user_id = actual_message['user_id']
+        except KeyError: 
+            print(f'KeyError!')
+            continue
+        print(f"Received request to restore all of {user_id}'s files")
+        archive_ids = get_archive_ids(user_id)
+        for id in archive_ids: 
+            initiate_job()
+        
+        try: # Delete message from SQS queue
+            response = sqs.delete_message(
+                QueueUrl=config.get('aws', 'SQSRestoreQueueUrl'), 
+                ReceiptHandle=receipt_handle)
+        except exceptions.ClientError as e: 
+            code = e.response['Error']['Code']
+            if code == 'ReceiptHandleIsInvalid': 
+                print({
+                    'code': 500, 
+                    'status': 'Server Error', 
+                    'message': f'Receipt Handle Is Invalid: {e}',
+                }) 
+            else: 
+                print({
+                    'code': 500, 
+                    'status': 'Server Error', 
+                    'message': f'An error occurred: {e}',
+                })  
+            continue 
 
-### EOF
+def get_archive_ids(user_id): 
+    try: 
+        response = dynamodb.query(
+            TableName=config.get('aws', 'DynamoDBTable'), 
+            IndexName=config.get('aws', 'DynamoDBIndex'),
+            Select='SPECIFIC_ATTRIBUTES', 
+            ProjectionExpression="results_file_archive_id", 
+            KeyConditionExpression="user_id = :u", 
+            ExpressionAttributeValues={
+                ":u": {"S": user_id}}, 
+            FilterExpression='attribute_not_exists(s3_key_result_file) and attribute_exists(results_file_archive_id)' 
+        )
+        return response['Items']
+    except ClientError as e: 
+        print(e)
+        return []
+
+def initiate_job(id, tier='Expedited'): 
+    try: 
+        response = glacier.initiate_job( # Initiate job to retrieve from glacier
+            vaultName=config.get('aws', 'S3GlacierBucketName'), 
+            jobParameters={
+                Type='archive-retrieval',
+                ArchiveId=id, 
+                SNSTopic=config.get('aws', 'SNSThawTopicARN'), 
+                Tier=tier, 
+                Outputlocation={
+                    'S3': {
+                        'BucketName': config.get('aws', 'ResultsBucket')
+                    }
+                }
+            }
+        )
+        print(f"    initiating {tier} job with jobId {response['jobId']}")
+    except glacier.exceptions.InsufficientCapacityException or glacier.exceptions.InsufficientCapacityException: 
+        initiate_job(id, tier='Standard')        
+
+
+if __name__ == '__main__': 
+    main()
